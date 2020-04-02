10b3af8bdadb3679954e69c2442a191b5821677c
Rhonda Kremer <rhondakremer@gmail.com>
2020-04-02 15:33:06 -0400

    profile responsive too, oops

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="container" class="col-lg-10 offset-lg-1 col-md-10 offset-md-1">
+  <div id="container" class="col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-12 col-xs-12">
     <img id ="mypic" src='../assets/myface.jpg' />
     <div id="brand-statement">
       <p>
-        Full Stack Web Developer with excellent teamwork skills and keen attention to detail. A graduate of the Full Stack Web Development Bootcamp at the University of Miami. Currently working as a front-end developer at Miami Dade County Public Schools. Analytical thinking skills and competence in various languages and tools such as Javascript, Node.js, Vue, React, and more. Prepared to create high-level and fully-functioning websites and applications.</p>
+        Full Stack Web Developer with excellent teamwork skills and keen attention to detail. Currently working as a front-end developer at Miami Dade County Public Schools. A graduate of the Full Stack Web Development Bootcamp at the University of Miami. Analytical thinking skills and competence in various languages and tools such as Javascript, Node.js, Vue, React, and more. Prepared to create high-level and fully-functioning websites and applications.</p>
       <p>
         <a href="https://github.com/rhondakremer" target="_blank"><img src="../assets/github-icon.png" class="icon" style="margin-left: -3%;" /></a>
         <a href="https://www.linkedin.com/in/rhondakremer/" target="_blank"><img src="../assets/linkedin-icon.png" class="icon" /></a>
@@ -36,7 +36,6 @@ export default {
 
 #brand-statement {
   line-height: 30px;
-
   margin-left: 2%;
   font-size: 18px;
 }
@@ -44,5 +43,40 @@ export default {
 .icon {
   width: 5%;
   margin-left: 3%;
+}
+
+@media screen and (max-width: 1024px) {
+  #mypic {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  #container {
+    display: block;
+  }
+  h2 {
+    font-size: 20px;
+    margin-top: 15px;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+
+  #mypic {
+    width: 80%;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  h2 {
+    font-size: 18px;
+    margin-top: 15px;
+  }
+  
+  .text {
+    font-size: 10px;
+  }
 }
 </style>
