e3d1f976d3db94377055b7b92dc6df6362cb7a70
Rhonda Kremer <rhonda.kremer@invitae.com>
2021-04-30 12:15:04 -0400

    delpoying

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import StripeProviderHolder from './components/StripeProviderHolder';
+
+function App() {
+
+  return (
+    <div>
+      <p>is anything working?</p>
+      <StripeProviderHolder />
+    </div>
+  );
+}
+
+export default App;
